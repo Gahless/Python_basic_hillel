b07400c3261966b7846ec6a4f2a918641971e9a7
Gahless <95930843+Gahless@users.noreply.github.com>
2022-09-18 15:04:24 +0300

    Reworked task2

--- HW_5th_lesson_14.09.2022/task_2.py
+++ HW_5th_lesson_14.09.2022/task_2.py
@@ -1,39 +1,15 @@
 
-        # Переменные названы соответственно их значению
-num_1 = float(input('Enter first number: '))
-num_2 = float(input('Enter second number: '))
-num_3 = float(input('Enter third number: '))
+a = float(input('Enter first number: '))
+b = float(input('Enter second number: '))
+c = float(input('Enter third number: '))
 
 max = ""
 
-if num_2 < num_1 > num_3 or num_2 == num_1 > num_3 or num_2 < num_1 == num_3 or num_2 == num_1 == num_3:
-    max =  num_1
-elif num_1 < num_2 > num_3 or num_1 < num_2 == num_3:
-    max = num_2
-elif num_1 < num_3 > num_2:
-    max = num_3
-else:
-    print('Unknown error')
-    exit()
+if b <= a >= c:
+    max = a
+elif a <= b >= c:
+    max = b
+elif a <= c >= b:
+     max = c
 
 print(max)
-
-        # Переменные названы соответственно условию
-# a = float(input('Enter first number: '))
-# b = float(input('Enter second number: '))
-# c = float(input('Enter third number: '))
-#
-# max = ""
-#
-# if b < a > c or b == a > c or b < a == c or b == a == c:
-#     max =  a
-# elif a < b > c or a < b == c:
-#     max = b
-# elif a < c > b:
-#     max = c
-# else:
-#     print('Unknown error')
-#     exit()
-#
-# print(max)
-
