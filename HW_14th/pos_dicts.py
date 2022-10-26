491cbd62fa8018de1ac5979b4d27b0c34a18cfab
Gahless <95930843+Gahless@users.noreply.github.com>
2022-10-26 07:00:57 +0300

    completing_HW14

---
+++ HW_14th/pos_dicts.py
@@ -0,0 +1,41 @@
+
+def arguments_validate(lst, *positions):
+    for pos in positions:  # Валидатор pos (целый int больше 0)
+        if (not isinstance(pos, int)) or pos % 1 != 0 or pos < 1:
+            raise ValueError('el_delete(lst, pos"), pos expected to be an integer and bigger than 0')
+
+    for el in lst:  # Валидатор элементов lst (только словари, только имеющие ключи position и name)
+        if not isinstance(el, dict):
+            raise ValueError(f'"{el}" is wrong element. el_delete(lst, pos"),'
+                             f'"lst" expected to contain only dictionaries')
+        if el.get('position') is None or el.get('name') is None:
+            raise ValueError(f'{el} expected to contain key "position" and key "name"')
+
+
+def el_delete(lst: list, pos: int = 1):
+    arguments_validate(lst, pos)
+
+    pos -= 1
+    del lst[pos]
+
+    for el in lst[pos:]:
+        el['position'] -= 1
+    return lst
+
+
+def el_insert(name, lst: list, pos: int):
+    arguments_validate(lst, pos)
+
+    lst.insert(pos - 1, {'name': name, 'position': pos})
+
+    for el in lst[pos:]:
+        el['position'] += 1
+    return lst
+
+
+def el_swap(lst: list, pos_1: int, pos_2: int):
+    arguments_validate(lst, pos_1, pos_2)
+
+    lst[pos_1 - 1]['name'], lst[pos_2 - 1]['name'] = lst[pos_2 - 1]['name'], lst[pos_1 - 1]['name']
+
+    return lst
---
+++ HW_14th/task_1_check.py
@@ -0,0 +1,15 @@
+import pos_dicts
+
+data = [
+    {'name': 'Test 1', 'position': 1},
+    {'name': 'Test 2', 'position': 2},
+    {'name': 'Test 3', 'position': 3},
+    {'name': 'Test 4', 'position': 4},
+    {'name': 'Test 5', 'position': 5},
+]
+
+print(pos_dicts.el_delete(data, 2))
+print()
+print(pos_dicts.el_insert('Test 2', data, 2))
+print()
+print(pos_dicts.el_swap(data, 1, 5))
